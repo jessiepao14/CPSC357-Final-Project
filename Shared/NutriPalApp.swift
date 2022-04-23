5f1e943fc22116fbfc407f46c8a5fcf112c1db25
jessiepao14 <jpao@chapman.edu>
2022-04-23 16:11:36 -0700

    Initial Commit

---
+++ Shared/NutriPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriPalApp.swift
+//  Shared
+//
+//  Created by Jessie Pao on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct NutriPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
